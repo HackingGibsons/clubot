75ed8a771a2c0e388075aaeed9d469e44559925a
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-01-01 23:07:40 -0500

    [build] Adding bundling

--- src/make/99-clean.mk
+++ src/make/99-clean.mk
@@ -1,2 +1,2 @@
 clean-all: | clean
-clean: | lisp-fasl-clean asdf-clean lisp-clean externals-clean clean-clubot clean-buildapp
+clean: | lisp-fasl-clean asdf-clean lisp-clean externals-clean clean-clubot clean-buildapp clean-all-bundle
