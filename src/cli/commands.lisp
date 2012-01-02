1b1dfeb6590771886aa25083f943df0bd4048b89
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-01-01 21:54:20 -0500

    [cli] Start command

--- src/cli/commands.lisp
+++ src/cli/commands.lisp
@@ -1,5 +1,20 @@
 (in-package :clubot-cli)
 
+(defcommand start (argv)
+  "Start a Clubot server."
+  (with-cli-options (argv "Usage: start [options]~%~@{~A~%~}~%")
+      ((logging "Enable logging")
+       &parameters (nick "The nick the bot should have.")
+                   (server "The IRC host to connect to")
+                   (port "The IRC port to use"))
+    (let ((nick (or nick "clubot"))
+          (server (or server "localhost")) (port (or (parse-integer port :junk-allowed t) 6667)))
+
+      (when logging
+        (clubot:start-logging :output t :warning t :trace nil))
+
+      (clubot:clubot :nick nick :host server :port port))))
+
 (defcommand repl (argv)
   "Start a REPL, or if forms are provided, evaluate the forms and terminate."
   (with-cli-options (argv "Usage: repl [options] [form1 form2 form3]~%~@{~A~%~}~%")
