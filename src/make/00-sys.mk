1c011bc51b93c28e1ac6ad45406c2542f5ffea96
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-01-01 19:27:37 -0500

    [master] Adds a submodule init step and made the dependencies in 00-sys order-only. Fixes #6

--- src/make/00-sys.mk
+++ src/make/00-sys.mk
@@ -5,13 +5,21 @@ VERBOSITY ?= 2 # [0, 3]
 # Helper
 SHUTUP = > /dev/null 2> /dev/null
 
-init: sanity-check quicklisp externals
+.PHONY: init
+init: | sanity-check quicklisp submodules externals
 
+.PHONY: | sanity-check
 sanity-check: $(ROOT)/$(TARGET).asd $(LISP_BIN) $(QL_SETUP)
 	@echo "!> Environment looks sane. I'll allow this."
 
-externals:
+.PHONY: externals
+externals: | submodules
 	$(MAKE) -C $(ROOT)/vendor all
 
+.PHONY: externals-clean
 externals-clean:
 	$(MAKE) -C $(ROOT)/vendor clean
+
+.PHONY: submodules
+submodules:
+	(cd $(ROOT); git submodule update --init --recursive;)
